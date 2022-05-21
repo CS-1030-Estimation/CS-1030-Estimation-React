9e714d30b9b1165f2d334626e915bfe378c27576
skylar siggard <siggardskylar@gmail.com>
2022-05-21 10:16:19 -0600

    add

--- src/Componants/Estimate.jsx
+++ src/Componants/Estimate.jsx
@@ -1,12 +1,10 @@
 import React, {componants} from 'react';
-import Create from './Create'
 
 
 function Estimate() {
   return (
     <div className="Estimate">
       <h1>Esimate.jsx</h1>
-      <Create/>
     </div>
   );
 }
