7b13ae3c602c0e5efdfe8818f500b36fa97dc109
unknown <siggardskylar@gmail.com>
2022-06-04 17:19:12 -0600

    finished with all input related things

--- src/Componants/edits/edits.jsx
+++ src/Componants/edits/edits.jsx
@@ -1,39 +1,90 @@
 import React, { Component } from 'react'
-// import Select from 'react-select'
+import Select from "react-select"
 
 export default class Edits extends Component {
-
     state = {
         edit: false,
-        input: this.props.text
+        input: this.props.text,
+        sf: this.props.sf,
+        material: [{id:10,text:"Brick",sf:500}],
+        select: {
+            value: null
+        }
     }
-    
-    handleChange = (e) => {
+
+    handleNumber = (e) => {
         this.setState({
-        input: e.target.value
+            sf: e.target.value
         })
-    }
-    
+    } 
+
     handleEdit = () => {
         this.setState({
             edit: false
         })
-        this.props.handleEdit(this.props.id, this.state.input)
+        this.props.handleEdit(this.props.id, this.state.input, this.state.sf)
+    }
+
+    toggleEdit = () => {
+        const options = [
+            {value:'Brick',lable:'Brick'},
+            {value:'Stone',lable:'Stone'},
+            {value:'Stucco',lable:'Stucco'},
+            {value:'Lap',lable:'Lap'},
+            {value:'Panel',lable:'Panel'}
+            ]
+            for (let i = 0; i < options.length; i++) {
+                if (this.props.text === options[i].lable){
+                    console.log(this.props.text, options[i].lable)
+                    this.setState({
+                        edit: !this.state.edit,
+                        select: {
+                            value: options[i],
+                            options
+                        }
+                    })
+                }
+            }
+    }
+
+    setValue = value => {
+        this.setState(prevState => ({
+            select: {
+            ...prevState.select,
+            value
+            }
+        }))
     }
     
-    toggleEdit = () => {
+    handleText = value => {
+        this.setValue(value)
         this.setState({
-        edit: !this.state.edit
+            input: value.lable
+        })
+    }
+    
+    handleNumber = (e) => {
+        this.setState({
+            sf: e.target.value
         })
     }
 
     render = () => {
+
+        const { select } = this.state;
+
         return ( 
         <div>
             {!this.state.edit ? <>{this.props.text}</> :
             <div>
-                <input onChange={(e) => this.handleChange(e)} value={this.state.input} type='text'/>
-                <button onClick={this.handleEdit}>Submit</button>
+                <Select 
+                getOptionLabel={(options) => options['lable']}
+                options={select.options} 
+                value={select.value}
+                onChange={this.handleText}
+                />
+                <input onChange={(e) => this.handleNumber(e)} type='number' min='0' placeholder='Squre Foot' value={this.state.sf}/>
+            <button onClick={this.handleEdit}>Submit</button>
             </div>}
             <button onClick={this.toggleEdit}>Edit</button>
             <button onClick={() => this.props.handleDelete(this.props.id)}>Delete</button>
--- src/Componants/inputs/inputs.jsx
+++ src/Componants/inputs/inputs.jsx
@@ -1,36 +1,46 @@
 import React, { Component } from 'react'
 import Edits from './../edits/edits'
 import Estimate from '../estimate/estimate'
-// import Select from 'react-select'
+import Select from "react-select"
 
 export default class Inputs extends Component {
   constructor(){
     super()
+    const options = [
+      {value:'Brick',lable:'Brick'},
+      {value:'Stone',lable:'Stone'},
+      {value:'Stucco',lable:'Stucco'},
+      {value:'Lap',lable:'Lap'},
+      {value:'Panel',lable:'Panel'}
+    ]
+
     this.state = {
       key: 1,
       input: '',
-      material: [{id:10,text:"Brick"}]
+      sf: 0,
+      material: [{id:10,text:"Brick",sf:500}],
+      select: {
+        value: null,
+        options
+      }
     }
   }
 
-  handleChange = (e) => {
-    this.setState({
-      input: e.target.value
-    })
-  }
-
-  handleSumbit = (e) => {
-    this.state.material.push({id:this.state.key,text:e})
+  handleSumbit = (e,el) => {
+    this.state.material.push({id:this.state.key,text:e,sf:parseInt(el)})
     this.setState((prevState) => ({
       key: prevState.key + 1,
-      input: ''
+      input: '',
+      sf: 0
     }))
+    this.setValue(null)
   }
 
-  handleEdit = (e , text) => {
+  handleEdit = (e , text, sf) => {
     for (let i = 0; i < this.state.material.length; i++) {
       if (this.state.material[i].id === e) {
         this.state.material[i].text = text
+        this.state.material[i].sf = parseInt(sf)
       } 
     }
     this.setState({
@@ -49,9 +59,31 @@ export default class Inputs extends Component {
     })
   }
 
+  setValue = value => {
+    this.setState(prevState => ({
+      select: {
+        ...prevState.select,
+        value
+      }
+    }))
+  }
+
+  handleText = value => {
+    this.setValue(value)
+    this.setState({
+      input: value.lable
+    })
+  }
+
+  handleNumber = (e) => {
+    this.setState({
+      sf: e.target.value
+    })
+  }
 
   render(){
 
+    const { select } = this.state;
 
     let list = this.state.material.map(element => {
       return <Edits
@@ -59,13 +91,20 @@ export default class Inputs extends Component {
         handleEdit={this.handleEdit}
         key={element.id}
         id={element.id}
-        text={element.text} />
+        text={element.text} 
+        sf={element.sf}/>
     })
 
     return (
       <div>
-        <input value={this.state.input} onChange={(e) => this.handleChange(e, 'input')} type='text'/>
-        <button onClick={() => this.handleSumbit(this.state.input)}>Add Items</button>
+        <Select 
+        getOptionLabel={(options) => options['lable']}
+        options={select.options} 
+        value={select.value}
+        onChange={this.handleText}
+        />
+        <input onChange={(e) => this.handleNumber(e)} type='number' min='0' placeholder='Squre Foot' value={this.state.sf}/>
+        <button onClick={() => this.handleSumbit(this.state.input, this.state.sf)}>Add Items</button>
         {list} 
         {this.state.material.length > 0 &&
           <Estimate material={this.state.material} />
